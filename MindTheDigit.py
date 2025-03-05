85caf38457cded92a308c7a341130d47747eecda
Pasindu Dulanjana <pasindu@agrithmics.lk>
2025-03-06 00:17:47 +0530

    Implent User Choice.

--- MindTheDigit.py
+++ MindTheDigit.py
@@ -13,5 +13,15 @@ def guessTheNumber(x):
         except ValueError:
                 print("Invalid input... Please enter a valid number.")
     print(f"Congrats. You guessed the number {randomNumber} correctly..!")
+    
+    while True:
+        userChoice = input("Are you sure want to run this program again (Y/N) :").strip().upper() # .strip() for Remove starting ending spaces and .upper() use convert to upper case
+        if (userChoice=="Y") :
+            guessTheNumber(10)
+        elif (userChoice=="N"):
+            print("Thank You..!")
+            break
+        else :
+            print("Invalid input...")
             
 guessTheNumber(10)
